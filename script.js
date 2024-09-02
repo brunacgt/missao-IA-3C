a16c07ffc89700cfb6f68c910cf6823fef3fe5d3
brunacgt <117641824+brunacgt@users.noreply.github.com>
2024-09-02 10:15:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,68 +8,6 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
-const perguntas = [
-    {
-        enunciado: "Pergunta 1",
-        alternativas: [
-            {
-                texto: "Alternativa 1 da pergunta 1",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto: "Alternativa 2 da pergunta 1",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }           
-            
-        ]
-    },
-    {
-        enunciado: "Pergunta 2",
-        alternativas: [
-            {
-                texto:"Alternativa 1 da pergunta 2",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto: "Alternativa 2 da pergunta 2",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
-        ]
-    },
-    {
-        enunciado: "Pergunta 3",
-        alternativas: [
-            {
-                texto:"Alternativa 1 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto:"Alternativa 2 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
-            
-        ]
-    },
-];
-
 let atual = 0; 
 let perguntaAtual;
 let historiaFinal = "";
