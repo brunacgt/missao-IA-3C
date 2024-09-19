f776caa086de36f1366020c762db1dd4e6f4e242
brunacgt <117641824+brunacgt@users.noreply.github.com>
2024-09-19 07:40:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,25 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-const botaoJogarNovamente = document.querySelector(".novamente-btn");
+const botaoJogarNovamente = document.querySelector(".novamente-btn"); 
+const botaoIniciar = document.querySelector(".iniciar-btn");
+const telaInicial = document.querySelector(".tela-inicial");
 
 let atual = 0; 
 let perguntaAtual;
 let historiaFinal = "";
+
+botaoIniciar.addEventListener('click', iniciaJogo);
+
+function iniciaJogo() {
+    atual = 0;
+    historiaFinal = "";
+    telaInicial.style.display = 'none';
+    caixaPerguntas.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultado.classList.remove("mostrar");
+    mostraPergunta();
+}
 
 function mostraPergunta() {
     if(atual >= perguntas.length){
@@ -40,15 +54,23 @@ function respostaSelecionada(opcaoSelecionada){
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = `Em 2049, &{nome}`/*add 12/09*/
     textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = ""; 
-    botaoJogarNovamente.addEventListentener("click", jogarNovamente());
+    caixaAlternativas.textContent = "";
+    caixaResultado.classList.add("mostrar"); 
+    botaoJogarNovamente.addEventListener("click", jogarNovamente); 
 }
-function jogarNovamente()}
-    atual= 0;
+
+function jogarNovamente(){
+    atual = 0;
     historiaFinal = "";
+    caixaResultado.classList.remove("mostrar"); 
     mostraPergunta();
 }
 
-mostraPergunta();
+function substituiNome() {
+    for(const pergunta of perguntas) {
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
+    }
+}
+substituiNome();
